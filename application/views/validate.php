47e06a443f3620b51d7dfb107e1b4d39898d7a1a
Philip Ashlock <phil@ashlock.us>
2016-07-15 18:07:22 -0700

    Remove options for JSON text or file upload

--- application/views/validate.php
+++ application/views/validate.php
@@ -85,61 +85,6 @@
                 </div>
             </form>
 
-            <hr>
-
-            <h3 style="margin-top : 3em;">Validate JSON file upload</h3>
-
-            <form action="<?php echo site_url(); ?>validate" method="post" enctype="multipart/form-data" role="form">
-
-                <div class="form-group">
-                    <label for="datajson">Schema</label>
-                    <select name="schema">
-                       <option value="georeport-v2/requests.json">GeoReport Requests</option>                        
-                       <option value="georeport-v2/services.json">GeoReport Services</option>                        
-                       <option value="georeport-v2/service-definition.json">GeoReport Service Definition</option>   
-                    </select>
-                </div>
-
-                 <div class="form-group">
-                    <label for="datajson_upload">Upload a JSON file</label>
-                    <input type="file" name="datajson_upload">
-                </div>
-
-                <div class="form-group">
-                    <input name="qa" value="true" type="hidden">
-                    <input type="hidden" name="output" value="browser">
-                    <input type="submit" value="Validate File" class="btn btn-primary">
-                </div>
-
-            </form>
-
-            <hr>
-
-            <h3 style="margin-top : 3em;">Validate raw JSON</h3>
-
-            <form action="<?php echo site_url(); ?>validate" method="post" role="form">
-                <div class="form-group">
-                    <label for="datajson">JSON</label>
-                    <textarea class="form-control" id="datajson" name="datajson" style="height : 30em; width: 100%"></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label for="datajson">Schema</label>
-                    <select name="schema">
-                       <option value="georeport-v2/requests.json">GeoReport Requests</option>                        
-                       <option value="georeport-v2/services.json">GeoReport Services</option>                        
-                       <option value="georeport-v2/service-definition.json">GeoReport Service Definition</option>   
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <input name="qa" value="true" type="hidden">
-                    <input type="hidden" name="output" value="browser">
-                    <input type="submit" value="Validate JSON" class="btn btn-primary">
-                </div>
-
-            </form>
-
         </div>
     </div>
 
